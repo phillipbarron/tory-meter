c5f5e77bb6891a0eced302211d8ee4043256c5d5
Phill Barron <phillipbarron242@gmail.com>
2019-07-28 13:56:32 +0100

    added react bootstrap and text imput eith button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous"
+      />
       <header className="App-header">
-        <p>OK, so now we just need to do some AI</p>
-        <p>How hard can it be?</p>
+        <Form>
+          <Form.Group controlId="articleContent">
+            <Form.Label>Article content</Form.Label>
+            <Form.Control as="textarea" rows="3" />
+            <Button variant="success">Detect Toriness</Button>
+          </Form.Group>
+        </Form>
       </header>
     </div>
   );
