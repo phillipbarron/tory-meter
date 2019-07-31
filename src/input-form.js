310b176bff57a49f73f80d548b87874afa825688
Phill Barron <phillipbarron242@gmail.com>
2019-07-31 21:20:10 +0100

    layout and that

--- src/input-form.js
+++ src/input-form.js
@@ -1,23 +1,34 @@
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 import Button from 'react-bootstrap/Button';
 import isRightWing from './sentiment-service'
 
 function InputForm() {
     const [isTory, setTory] = useState(undefined);
+    const [articleContent, setContent] = useState(undefined);
     return (
-        <Form>
-          <Form.Group controlId="articleContent">
-            <Form.Label>Article content</Form.Label>
-            <Form.Control as="textarea" rows="3" />
-            <Button variant="success" onClick={() => setTory(isRightWing())} >Detect Toriness</Button>
-            {isTory !== undefined &&
-              <div>
-                {isTory ? "Tory" : "Normal" }
-              </div>
-            }
-          </Form.Group>
-        </Form>
+        <Container>
+            <Form>
+                <Form.Group controlId="articleContent">
+                <Row>
+                    <Form.Label>Article content</Form.Label>
+                    <Form.Control as="textarea" rows="3" value={articleContent} onChange={ value => setContent(value.value)} /> 
+                </Row>
+                <Row className="input-form__button">
+                    <Button variant="success" onClick={() => setTory(isRightWing())}>Detect Toriness</Button>
+                </Row>
+                <Row className="input-form__result">
+                    {isTory !== undefined &&
+                        <div>
+                        {isTory ? "Tory" : "Normal" }
+                        </div>
+                    }
+                </Row>
+                </Form.Group>
+            </Form>
+        </Container>
     );
 }
 
