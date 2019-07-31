798f51b1533d0b43f89f782ed85e43f7a97b1c1c
Phill Barron <phillipbarron242@gmail.com>
2019-07-31 18:48:13 +0100

    added fake detector and some state to use it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import isRightWing from './sentiment-service'
 import './App.css';
 
 function App() {
+  const [isTory, setTory] = useState(undefined);
   return (
     <div className="App">
       <link
@@ -17,7 +19,12 @@ function App() {
           <Form.Group controlId="articleContent">
             <Form.Label>Article content</Form.Label>
             <Form.Control as="textarea" rows="3" />
-            <Button variant="success">Detect Toriness</Button>
+            <Button variant="success" onClick={() => setTory(isRightWing())} >Detect Toriness</Button>
+            {isTory !== undefined &&
+              <div>
+                {isTory ? "Tory" : "Normal" }
+              </div>
+            }
           </Form.Group>
         </Form>
       </header>
